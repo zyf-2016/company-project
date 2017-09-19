d0adaf5ba7c93dfbd7a1662d117a2968b47cb200
zyf-2016 <1178356585@qq.com>
2017-09-19 10:49:40 +0800

    finished

---
+++ js/slide.js
@@ -0,0 +1,106 @@
+//存储了每个图片的信息
+var json = [
+	{
+		width:196,
+		top:20,
+		left:0,
+		top: 0,
+//		opacity:20,
+		z:2
+	},
+	{
+		width:212,
+		top:24,
+		left:146,
+//		opacity:80,
+		z:3
+	},
+	{
+		width:250,
+		top:48,
+		left:270,
+//		opacity:100,
+		z:4
+	},
+	{
+		width:212,
+		top:24,
+		left:432,
+//		opacity:80,
+		z:3
+	},
+	{
+		width:196,
+		top:0,
+		left:596,
+//		opacity:20,
+		z:2
+	}
+];
+
+var wrap = document.querySelector(".wrap"); //大盒子
+var arrow = document.querySelector(".arrow"); //三角
+var slider = document.getElementById("slide");//获得图片的盒子
+var lis = document.querySelectorAll("li");//所有要操作的图片
+
+//wrap.onmouseover = function() {  // 鼠标经过显示和隐藏 左右两个箭头
+//   animate(arrow,{opacity:100});
+//}
+//wrap.onmouseout = function() {
+//  animate(arrow,{opacity:0});
+//}
+
+//  两个按钮点击事件
+// 函数节流
+var jieliu = true; //  用来控制函数节奏的 变量
+var as = arrow.children;
+change();
+for(var k in as)
+{
+    as[k].onclick = function() {
+        if(this.className == "prev")
+        {
+            //  alert("左侧")  移除第一个   放到json 最后一个
+            if(jieliu == true)
+            {
+                change(false);
+                jieliu = false;  // 点击完毕之后，立马取反
+            }
+
+        }
+        else
+        {
+           // alert('右侧');   把 最后一个json 删除   并且把最后一个添加到json 第一个位置
+            if(jieliu == true)
+            {
+                change(true);
+                jieliu = false;  // 点击完毕之后，立马取反
+            }
+        }
+    }
+}
+
+function change(flag) {
+    //  来判断
+    if(flag)
+    {
+        // 把 最后一个json 删除   并且把最后一个添加到json 第一个位置
+         json.unshift(json.pop());
+    }
+    else
+    {
+        //   移除第一个   放到json 最后一个
+        json.push(json.shift());
+    }
+  // 为什么给for呢？ 因为我们的json 有5个  盒子li 有 5个
+    for(var i=0;i<json.length; i++)
+    {
+        animate(lis[i],{
+            width: json[i].width,
+            top: json[i].top,
+            left: json[i].left,
+            opacity:json[i].opacity,
+            zIndex:json[i].z
+        },function(){ jieliu = true;})  // 回调函数是等动画执行完毕  才行
+    }
+}
